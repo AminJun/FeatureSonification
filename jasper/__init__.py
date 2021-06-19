3b614e482626b73bd6ec37686a0e3efd79ddd20a
AminJun <mohammad.amin.ghiasi@gmail.com>
2021-06-19 13:45:23 -0400

    Merged the jasper into one

--- jasper/__init__.py
+++ jasper/__init__.py
@@ -4,7 +4,7 @@ import os
 from .model import JasperEncoderDecoder as _Jasper
 from .jasper_augs import JasperAugs as _Aug
 
-__all__ = ['jasper', 'to_mel']
+__all__ = ['Jasper']
 
 
 def jasper() -> nn.Module:
@@ -19,3 +19,18 @@ def to_mel() -> nn.Module:
     checkpoint = torch.load(os.path.join('checkpoints', 'to_mel.pt'), map_location="cpu")
     network.load_state_dict(checkpoint, strict=False)
     return network
+
+
+class Jasper(nn.Module):
+    def __init__(self, classifier: nn.Module = None, augmentations: nn.Module = None):
+        super().__init__()
+        if classifier is None:
+            classifier = jasper()
+        if augmentations is None:
+            augmentations = to_mel()
+        self.augmentations = augmentations
+        self.classifier = classifier.module if isinstance(classifier, nn.DataParallel) else classifier
+
+    def forward(self, *inputs: [torch.Tensor]) -> [torch.Tensor]:
+        augmented = self.augmentations(*inputs)
+        return self.classifier(augmented)
