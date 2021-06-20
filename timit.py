be7390974b872ef4b17273badb08442bda160672
AminJun <mohammad.amin.ghiasi@gmail.com>
2021-06-19 20:37:20 -0400

    Revert back to our own timit loader

--- timit.py
+++ timit.py
@@ -1,19 +1,52 @@
+from pathlib import Path
+
 import numpy as np
 import torch
 import torchaudio
 from torch.utils.data.dataset import Dataset
 
+import os
+
+
+def load_timit(file: str):
+    data_path = os.path.splitext(file)[0]
+    with open(data_path + '.TXT', 'r') as txt_file:
+        _, __, transcript = next(iter(txt_file)).strip().split(" ", 2)
+    with open(data_path + '.WRD', 'r') as word_file:
+        words = [l.strip().split(' ') for l in word_file]
+        words = [(int(hd), int(tl), w) for (hd, tl, w) in words]
+    with open(data_path + '.PHN', 'r') as phn_file:
+        phonemes = [l.strip().split(' ') for l in phn_file]
+        phonemes = [(int(hd), int(tl), w) for (hd, tl, w) in phonemes]
+    wav, sr = torchaudio.load(data_path + '.WAV')
+    return data_path, wav, transcript, words, phonemes
+
+
+class Timit(Dataset):
+    def __init__(self, root: str):
+        self.root = root
+        self.walker = list(str(p.stem) for p in Path(self.root).glob('*/*/*/*/*' + '.WAV'))
+
+    def __getitem__(self, item) -> (str, torch.tensor, str, list, list):
+        return load_timit(self.walker[item])
+
+    def __len__(self) -> int:
+        return len(self.walker)
+
 
 class AbstractTimit(Dataset):
-    def __init__(self, parent: list, time: int = 7 * 8 * 320 - 1, subset: str = 'SI', key: str = 'word'):
-        # huggingface items key to word/phoneme dict per data.
-        self._key = f'{key}_detail'
-        """
-          The length of output audio clip must follow the follwing formula, 
-          Otherwise the Jasper's default augmentation will pad it with zeros and 
-          hence there will be a huge bias towards zero, which eventually it will 
-          degrade the quality of our sonification.  
-        """
+    def complete(self, wav: torch.tensor) -> torch.tensor:
+        # if wav.size(-1) > self.time:
+        #    raise Exception
+        return wav.repeat(1, int(np.ceil(self.time / wav.size(-1))))[:, :self.time].clone().detach()
+
+    def get_words(self, data: list, index: int) -> list:
+        wav = data[1].cuda()
+        return [(self.complete(wav[:, st:en]), w, index, st, en) for (st, en, w) in data[self._item_id] if
+                st != en and self.subset in data[0]]
+
+    def __init__(self, parent: Timit, time: int = 16000, subset: str = 'SI', debug: bool = False, item_id: int = 3):
+        self._item_id = item_id
         assert (time + 1) % (320 * 8) == 0, f'time={time}+1 must be multiples of 320*8 bc of Jasper'
         self.data = []
         self.time = time
@@ -22,20 +55,8 @@ class AbstractTimit(Dataset):
         with torch.no_grad():
             for i, data in enumerate(parent):
                 self.data += self.get_words(data, i)
-
-    def complete(self, wav: torch.tensor) -> torch.tensor:
-        if wav.size(-1) > self.time:
-            raise Exception
-        return wav.repeat(1, int(np.ceil(self.time / wav.size(-1))))[:, :self.time].clone().detach()
-
-    def get_words(self, data: list, index: int) -> list:
-        if data['sentence_type'] != self.subset:
-            return []
-        wav, sr = torchaudio.load(data['file'])
-        wav = wav.view(-1)
-        real_data = data[self._key]
-        real_data = zip(real_data['start'], real_data['stop'], real_data['utterance'])
-        return [(self.complete(wav[st:en]), w, index, st, en) for (st, en, w) in real_data if st != en]
+                if debug and i > 3000:
+                    break
 
     def __getitem__(self, item):
         return self.data[item]
@@ -45,10 +66,10 @@ class AbstractTimit(Dataset):
 
 
 class TimitWord(AbstractTimit):
-    def __init__(self, parent: Dataset, time: int = 7 * 8 * 320 - 1, subset: str = 'SI'):
-        super().__init__(parent, time, subset, key='word')
+    def __init__(self, parent: Timit, time: int = 16000 - 1, subset: str = 'SI', debug: bool = False):
+        super().__init__(parent, time, subset, debug, item_id=3)
 
 
 class TimitPhoneme(AbstractTimit):
-    def __init__(self, parent: Dataset, time: int = 7 * 8 * 320 - 1, subset: str = 'SI'):
-        super().__init__(parent, time, subset, key='phonetic')
+    def __init__(self, parent: Timit, time: int = 16000, subset: str = 'SI', debug: bool = False):
+        super().__init__(parent, time, subset, debug, item_id=4)
